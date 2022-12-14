51404fbea8307e71ca49d816e63a37655cb6579f
frankenstyll <nontapap.th@gmail.com>
2022-12-14 22:31:22 +0700

    edit

--- src/main/java/com/example/demo/service/Consumer.java
+++ src/main/java/com/example/demo/service/Consumer.java
@@ -3,10 +3,12 @@ package com.example.demo.service;
 import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.annotation.KafkaListener;
+
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+
+//import org.springframework.kafka.annotation.KafkaListener;
 
 @Slf4j
 @Service
@@ -16,14 +18,14 @@ public class Consumer {
     private final String GROUP_ID = "ONELYNK_GROUP";
 
     //@KafkaListener(topics = "ONELYNK1", groupId = GROUP_ID)
-    @KafkaListener(topics = "ONELYNK1")
-    public void consume1(String message) throws IOException {
-        log.info("Consume1 Message: {}", message);
-    }
-
-    //@KafkaListener(topics = "ONELYNK2", groupId = GROUP_ID)
-    @KafkaListener(topics = "ONELYNK2")
-    public void consume2(String message) throws IOException {
-        log.info("Consume2 Message: {}", message);
-    }
+//    @KafkaListener(topics = "ONELYNK1")
+//    public void consume1(String message) throws IOException {
+//        log.info("Consume1 Message: {}", message);
+//    }
+//
+//    //@KafkaListener(topics = "ONELYNK2", groupId = GROUP_ID)
+//    @KafkaListener(topics = "ONELYNK2")
+//    public void consume2(String message) throws IOException {
+//        log.info("Consume2 Message: {}", message);
+//    }
 }
--- src/main/java/com/example/demo/service/Producer.java
+++ src/main/java/com/example/demo/service/Producer.java
@@ -23,12 +23,17 @@ public class Producer {
 
     private static String TOPIC1 = "ONELYNK1";
     private static String TOPIC2 = "ONELYNK2";
+
+    private int global_counter = 1;
+
     public void produce1(String data) {
         log.info("Produce Topic: {} - Message: {}", TOPIC1, data);
         Map<String, Object> req = new HashMap<>();
-        req.put("code", "0001");
+        req.put("code", global_counter++);
         req.put("message", "fail");
         req.put("method", "produce1");
+
+        log.info("req {}", gson.toJson(req) );
         this.kafkaTemplate.send(TOPIC1, gson.toJson(req));
     }
 
@@ -45,11 +50,11 @@ public class Producer {
         }
 
         Map<String, Object> req = new HashMap<>();
-        req.put("code", "0001");
+        req.put("code", global_counter++);
         req.put("message", "success");
         req.put("method", "produce2");
         req.put("data", lst);
-
+        log.info("req {}", gson.toJson(req) );
         this.kafkaTemplate.send(TOPIC2, gson.toJson(req) );
     }
 
